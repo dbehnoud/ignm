71f867a191b449813b275afb03e7661a8ddc78e1
dbehnoud <38480024+dbehnoud@users.noreply.github.com>
2023-01-07 16:29:57 -0800

    Update example file

--- sample/example.py
+++ sample/example.py
@@ -1,8 +1,12 @@
-from ignm.source.GasPhaseSolver import solver
+from ignm.gasPhaseSolver import solve
 import matplotlib.pyplot as plt
 
+input_filepath ='../data/input.yaml'
+ts_filepath = '../data/ts.csv'
+yin_filepath = '../data/yin.csv'
+j0_filepath = '../data/j0.csv'
 
-solution = solver('../data/example_input.yaml','../data/ts.csv','../data/yin.csv','../data/j0.csv' )
+solution = solve(input_filepath,ts_filepath,yin_filepath,j0_filepath)
 
 plt.plot(solution['time'], solution[solution.columns[-1]])
 plt.xlabel('time (s)')
